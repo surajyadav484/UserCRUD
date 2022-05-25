b3d9bbb378ac7088138ee02591dccea742185d0a
Suraj Yadav <surajryadav20@gmail.com>
2022-05-25 10:41:21 +0530

    Removed DB URL

--- UserManagementBackend/app.js
+++ UserManagementBackend/app.js
@@ -2,13 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/user");
 const bodyParser = require("body-parser");
+const dbURL = require("./utils/db");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(userRoutes);
-
-mongoose
-  .connect(
-    "mongodb+srv://suraj:Suraj123@cluster0.yizdy.mongodb.net/user?retryWrites=true&w=majority"
-  )
-  .then((result) => app.listen(8080));
+console.log(dbURL);
+mongoose.connect(dbURL.DB_URL).then((result) => app.listen(8080));
